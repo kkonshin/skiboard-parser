55948df83131cd1132efe7d1bec6c7891596ff4c
kkonshin <freekey@yandex.ru>
2018-10-19 10:01:30 +0300

    Скрипт отвязки товаров от всех разделов, кроме временного

---
+++ unbind.php
@@ -0,0 +1,34 @@
+#!/usr/bin/php
+
+<?php
+/*
+ * Скрипт активирует все товары во временном разделе skiboard temp. Для запуска набрать php -f activate.php
+ * в командной строке в папке парсера
+ */
+if (php_sapi_name() !== "cli") {
+	die ('Этот скрипт предназначен для запуска из командной строки');
+}
+
+require(__DIR__ . "/config.php");
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
+require_once("vendor/autoload.php");
+
+use Parser\SectionParams;
+use Parser\ItemsStatus;
+use Parser\BindToSections;
+
+while (ob_get_level()) {
+	ob_end_flush();
+}
+
+$params = new SectionParams(CATALOG_IBLOCK_ID, TEMP_CATALOG_SECTION);
+
+$itemStatus = new ItemsStatus($params);
+
+/*
+ * Отвязывает товары во временном разделе от всех остальных разделов
+ */
+
+BindToSections::bind($itemStatus, [TEMP_CATALOG_SECTION]);
+
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/epilog_after.php");
