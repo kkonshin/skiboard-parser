5d5f569015fae683bad586da8990c6f9fa207a1f
kkonshin <freekey@yandex.ru>
2018-11-02 12:25:13 +0300

    Повторный пуш Utils

---
+++ classes/Utils/Description.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Parser\Utils;
+
+class Description extends \Parser\ItemsStatus
+{
+	public static function updateDescription(\Parser\ItemsStatus $object, array $resultArray=null)
+	{
+		$itemsList = $object->getList();
+
+		try {
+			foreach ($itemsList as $itemKey => $itemValue) {
+				foreach ($resultArray as $resultArrayKey => $resultArrayValue){
+					if (\CUtil::translit($resultArrayValue[0]["NAME"] . ' ' . $resultArrayValue[0]["OFFER_ID"], "ru", P_TRANSLIT_PARAMS) === $itemValue["CODE"]){
+//						echo $resultArrayValue[0]["DESCRIPTION"] . PHP_EOL;
+						$element = new \CIBlockElement();
+						$element->Update($itemValue["ID"], ["DETAIL_TEXT" => $resultArrayValue[0]["DESCRIPTION"]]);
+					}
+				}
+			}
+		} catch (\Exception $exception){
+			echo "Обновить детальное описание не удалось" . PHP_EOL;
+			echo $exception->getMessage() . PHP_EOL;
+		}
+
+	}
+}
