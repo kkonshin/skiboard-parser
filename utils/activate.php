51b2b075c0ec763ede3ce4f37c4c562efcce353b
kkonshin <freekey@yandex.ru>
2019-04-11 12:50:41 +0300

    Исправлены пути во всех скриптах папки utils

--- utils/activate.php
+++ utils/activate.php
@@ -9,9 +9,9 @@ if (php_sapi_name() !== "cli") {
 	die ('Этот скрипт предназначен для запуска из командной строки');
 }
 
-require(__DIR__ . "/config.php");
+require(__DIR__ . "/../config.php");
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
-require_once("vendor/autoload.php");
+require_once(__DIR__ . "/../vendor/autoload.php");
 
 use Parser\SectionParams;
 use Parser\ItemsStatus;
