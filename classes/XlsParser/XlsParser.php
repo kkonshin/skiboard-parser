f395e12da299469b86244dcb94a68f1579bb3531
kkonshin <freekey@yandex.ru>
2019-05-24 13:07:45 +0300

    Исправлен баг с путем

--- classes/XlsParser/XlsParser.php
+++ classes/XlsParser/XlsParser.php
@@ -78,7 +78,7 @@ class XlsParser
             if (!is_file(self::XLS_FILE_PATH)) {
                 return;
             }
-            exec('cd ~/www/test/skiboard-parser-new/save; xls2csv sklad.xls > sklad.csv');
+            exec('cd ~/www/local/skiboard-parser-new/save; xls2csv sklad.xls > sklad.csv');
         } catch (\Exception $exception) {
             echo $exception->getMessage();
         }
